5686418b5d337927b923406e35bef071a8323c53
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2018-07-13 22:43:11 +0200

    add canvas

--- src/Countdown.vue
+++ src/Countdown.vue
@@ -2,16 +2,94 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Countdown"/>
+    <canvas ref="my-canvas"></canvas>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// https://alligator.io/vuejs/vue-html5-canvas/
+
 export default {
   name: 'countdown',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      // By creating the provider in the data property, it becomes reactive,
+      // so child components will update when `context` changes.
+      provider: {
+        // This is the CanvasRenderingContext that children will draw to.
+        context: null
+      }
+    }
+  },
+
+  // Allows any child component to `inject: ['provider']` and have access to it.
+  provide () {
+    return {
+      provider: this.provider
+    }
+  },
+
+  mounted () {
+    // We can't access the rendering context until the canvas is mounted to the DOM.
+    // Once we have it, provide it to all child components.
+    this.provider.context = this.$refs['my-canvas'].getContext('2d')
+
+    // Resize the canvas to fit its parent's width.
+    // Normally you'd use a more flexible resize system.
+    this.$refs['my-canvas'].width = this.$refs['my-canvas'].parentElement.clientWidth
+    this.$refs['my-canvas'].height = this.$refs['my-canvas'].parentElement.clientHeight
+  },
+  computed: {
+    calculatedBox () {
+      const ctx = this.provider.context
+
+      // Turn start / end percentages into x, y, width, height in pixels.
+      const calculated = {
+        x: percentWidthToPix(this.x1, ctx),
+        y: percentHeightToPix(this.y1, ctx),
+        w: percentWidthToPix(this.x2 - this.x1, ctx),
+        h: percentHeightToPix(this.y2 - this.y1, ctx)
+      }
+
+      // Yes yes, side-effects. This lets us cache the box dimensions of the previous render.
+      // before we re-calculate calculatedBox the next render.
+      this.oldBox = calculated
+      return calculated
+    }
+  },
+
+  render () {
+    // Since the parent canvas has to mount first, it's *possible* that the context may not be
+    // injected by the time this render function runs the first time.
+    if(!this.provider.context) return;
+    const ctx = this.provider.context;
+
+    // Keep a reference to the box used in the previous render call.
+    const oldBox = this.oldBox
+    // Calculate the new box. (Computed properties update on-demand.)
+    const newBox = this.calculatedBox
+
+    ctx.beginPath();
+    // Clear the old area from the previous render.
+    ctx.clearRect(oldBox.x, oldBox.y, oldBox.w, oldBox.h);
+    // Clear the area for the text.
+    ctx.clearRect(newBox.x, newBox.y - 42, newBox.w, 100);
+
+    // Draw the new rectangle.
+    ctx.rect(newBox.x, newBox.y, newBox.w, newBox.h);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+
+    // Draw the text
+    ctx.fillStyle = '#000'
+    ctx.font = '28px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillText(Math.floor(this.value), (newBox.x + (newBox.w / 2)), newBox.y - 14)
   }
 }
 </script>
